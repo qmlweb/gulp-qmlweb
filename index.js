qmlweb/gulp-qmlweb

commit 3151a56e5fb11fac056d8fe3e5a79b7ea0790e6b

Port to qmlweb-parser.
Fixes #4

index.js
@@ -1,8 +1,6 @@
 var es = require('event-stream');
 var gutil = require('gulp-util');
-require('qmlweb/src/qtcore/qml/QMLBinding.js');
-require('qmlweb/src/qtcore/qml/lib/parser.js');
-require('qmlweb/src/qtcore/qml/lib/jsparser.js');
+var qmlweb_parse = require('qmlweb-parser').qmlweb_parse;
 
 module.exports = function (opt) {
   function modifyFile(file) {
@@ -19,9 +17,9 @@ module.exports = function (opt) {
 
     try {
       if (file.path.match(/\.qml$/) != null)
-        data = qmlparse(str);
+        data = qmlweb_parse(str, qmlweb_parse.QMLDocument);
       else if (file.path.match(/\.js$/) != null)
-        data = jsparse(str);
+        data = qmlweb_parse(str, qmlweb_parse.JSResource);
       else
         data = str;
     } catch (err) {

commit ef1fd30edfd7d157c3dea76d934e5ad82a5ce582

Fix js files processing
I accidently broke it in 3151a56e5fb11fac056d8fe3e5a79b7ea0790e6b.
This utilizes the new qmlweb_jsparse() function from qmlweb-parser v0.2.

index.js
@@ -1,6 +1,6 @@
 var es = require('event-stream');
 var gutil = require('gulp-util');
-var qmlweb_parse = require('qmlweb-parser').qmlweb_parse;
+var parser = require('qmlweb-parser');
 
 module.exports = function (opt) {
   function modifyFile(file) {
@@ -17,9 +17,9 @@ module.exports = function (opt) {
 
     try {
       if (file.path.match(/\.qml$/) != null)
-        data = qmlweb_parse(str, qmlweb_parse.QMLDocument);
+        data = parser.qmlweb_parse(str, qmlweb_parse.QMLDocument);
       else if (file.path.match(/\.js$/) != null)
-        data = qmlweb_parse(str, qmlweb_parse.JSResource);
+        data = parser.qmlweb_jsparse(str);
       else
         data = str;
     } catch (err) {